450e4ddb2943736a868c4da37f7bf77aa3d7d105
Diego <diego.martin@aa.com>
2014-10-25 13:39:53 -0700

    fixed prices object

--- jqm/src/main/resources/static/js/pricing.js
+++ jqm/src/main/resources/static/js/pricing.js
@@ -2,22 +2,22 @@ var beachPriceList = [
     {
 
             "price": 390,
-            "outbound": {"fltNbr": 1364, "date": "2014-11-11", "time": "5:15 am", "arrivalTime": "8:15 am", "duration": "3 hrs"},
-            "inbound": {"fltNbr": 1364, "date": "2014-11-15", "time": "9:20 am", "arrivalTime": "12:20 pm", "duration": "3 hr 20 min"}
+            "outbound": {"fltNbr": 1364, "date": "2014-11-11", "time": "05:15 am", "arrivalTime": "06:25 am", "duration": "1 hr 10 min"},
+            "inbound": {"fltNbr": 1364, "date": "2014-11-15", "time": "09:20 am", "arrivalTime": "10:30 am", "duration": "1 hr 20 min"}
 
     },
     {
 
             "price": 450,
-            "outbound": {"fltNbr": 942, "date": "2014-11-11", "time": "7:40 am", "arrivalTime": "10:30 am", "duration": "3 hrs"},
-            "inbound": {"fltNbr": 533, "date": "2014-11-15", "time": "10:45 am", "arrivalTime": "2:05 pm",  "duration": "3 hr 20 min"}
+            "outbound": {"fltNbr": 942, "date": "2014-11-11", "time": "07:40 am", "arrivalTime": "08:50 am", "duration": "1 hr 10 min"},
+            "inbound": {"fltNbr": 533, "date": "2014-11-15", "time": "10:45 am", "arrivalTime": "11:55 pm",  "duration": "1 hr 20 min"}
 
     },
     {
 
             "price": 490,
-            "outbound": {"fltNbr": 206, "date": "2014-11-11", "time": "12:00 pm", "arrivalTime": "3:00 pm", "duration": "3 hrs"},
-            "inbound": {"fltNbr": 210, "date": "2014-11-15", "time": "1:45 pm", "arrivalTime": "5:05 pm", "duration": "3 hr 20 min"}
+            "outbound": {"fltNbr": 206, "date": "2014-11-11", "time": "12:00 pm", "arrivalTime": "01:10 pm", "duration": "1 hr 10 min"},
+            "inbound": {"fltNbr": 210, "date": "2014-11-15", "time": "01:45 pm", "arrivalTime": "03:05 pm", "duration": "1 hr 20 min"}
 
     }
 ]
@@ -25,19 +25,19 @@ var beachPriceList = [
 var romanticPriceList = [
     {
         "price": 393,
-        "outbound": {"fltNbr": 1108, "date": "2014-11-11", "time": "7:15 am", "arrivalTime": "9:15 am", "duration": "2 hrs"},
-        "inbound": {"fltNbr": 1364, "date": "2014-11-15", "time": "6:20 am", "arrivalTime": "8:30 am", "duration": "2 hr 10 min"}
+        "outbound": {"fltNbr": 1108, "date": "2014-11-11", "time": "07:15 am", "arrivalTime": "09:15 am", "duration": "2 hrs"},
+        "inbound": {"fltNbr": 1364, "date": "2014-11-15", "time": "06:20 am", "arrivalTime": "08:30 am", "duration": "2 hr 10 min"}
 
     },
     {
         "price": 401,
-        "outbound": {"fltNbr": 1501, "date": "2014-11-11", "time": "8:40 am", "arrivalTime": "11:40 am", "duration": "2 hrs"},
-        "inbound": {"fltNbr": 533, "date": "2014-11-15", "time": "7:45 am", "arrivalTime": "10:00 am", "duration": "2 hr 10 min"}
+        "outbound": {"fltNbr": 1501, "date": "2014-11-11", "time": "08:40 am", "arrivalTime": "11:40 am", "duration": "2 hrs"},
+        "inbound": {"fltNbr": 533, "date": "2014-11-15", "time": "07:45 am", "arrivalTime": "10:00 am", "duration": "2 hr 10 min"}
     },
     {
         "price": 460,
-        "outbound": {"fltNbr": 1120, "date": "2014-11-11", "time": "11:45 pm", "arrivalTime": "3:00 pm", "duration": "2 hrs"},
-        "inbound": {"fltNbr": 1138, "date": "2014-11-15", "time": "9:45 am", "arrivalTime": "10:00 am", "duration": "2 hr 10 min"}
+        "outbound": {"fltNbr": 1120, "date": "2014-11-11", "time": "11:45 pm", "arrivalTime": "03:00 pm", "duration": "2 hrs"},
+        "inbound": {"fltNbr": 1138, "date": "2014-11-15", "time": "09:45 am", "arrivalTime": "10:00 am", "duration": "2 hr 10 min"}
     }
 ]
 
