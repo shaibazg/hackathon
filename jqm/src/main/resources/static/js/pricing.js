c607720c7e738b878d339d969eb3aaebc783cb68
unknown <667340@QJ07300087.corpaa.aa.com>
2014-10-25 10:55:57 -0500

    pricing changes

--- jqm/src/main/resources/static/js/pricing.js
+++ jqm/src/main/resources/static/js/pricing.js
@@ -1,47 +1,20 @@
-var priceList = {"price":[
-{"destinationLocation": "MIA",
-"fareInfo": [
-  {"fltNbr": 1364, "LowestFare": 100.45, "deptTime": "5:15am", "returnDate": "2014-11-11", "returnTime": "9:00am"},
-  {"fltNbr": 942, "LowestFare": 200.45, "time": "7:40am", "returnDate": "2014-11-12", "returnTime": "9:00am"},
-  {"fltNbr": 942, "LowestFare": 300.45, "time": "12:00pm", "returnDate": "2014-11-13", "returnTime": "9:00am"},
-  {"fltNbr": 942, "LowestFare": 400.45, "time": "1:50pm", "returnDate": "2014-11-14", "returnTime": "9:00am"}
- ]
-},
-{"destinationLocation": "CUN",
-	"fareInfo": [
-	  {"fltNbr": 82, "LowestFare": 293.45, "time": "5:15am", "returnDate": "2014-11-11", "returnTime": "9:00am"},
-	  {"fltNbr": 1501, "LowestFare": 400.23, "time": "7:40am", "returnDate": "2014-11-12", "returnTime": "10:00am"},
-	  {"fltNbr": 1211, "LowestFare": 430.00, "time": "12:00pm", "returnDate": "2014-11-14", "returnTime": "9:00am"},
-	  {"fltNbr": 165, "LowestFare": 500.10, "time": "1:50pm", "returnDate": "2014-11-17", "returnTime": "9:00am"}
-	 ]
-}
-]}
-
 var beachPriceList = {"fareInfo": [
-                                   {"fltNbr": 1364, "LowestFare": 100.45, "time": "5:15am", "returnDate": "2014-11-11", "returnTime": "9:00am"},
-                                   {"fltNbr": 942, "LowestFare": 200.45, "time": "7:40am", "returnDate": "2014-11-12", "returnTime": "9:00am"},
-                                   {"fltNbr": 942, "LowestFare": 300.45, "time": "12:00pm", "returnDate": "2014-11-13", "returnTime": "9:00am"},
-                                   {"fltNbr": 942, "LowestFare": 400.45, "time": "1:50pm", "returnDate": "2014-11-14", "returnTime": "9:00am"}
+                                   {"fltNbr": 1364, "LowestFare": 590, "time": "5:15 am", "returnDate": "2014-11-11", "returnTime": "9:20 am"},
+                                   {"fltNbr": 942, "LowestFare": 550, "time": "7:40 am", "returnDate": "2014-11-12", "returnTime": "10:45 am"},
+                                   {"fltNbr": 206, "LowestFare": 715, "time": "12:00 pm", "returnDate": "2014-11-13", "returnTime": "1:45 pm"},
+                                   {"fltNbr": 60, "LowestFare": 650, "time": "1:50 pm", "returnDate": "2014-11-14", "returnTime": "6:30 pm"}
                                   ]
 					}
 
 var romanticPriceList = {"fareInfo": [
-                                	  {"fltNbr": 82, "LowestFare": 293.45, "time": "5:15am", "returnDate": "2014-11-11", "returnTime": "9:00am"},
-                                	  {"fltNbr": 1501, "LowestFare": 400.23, "time": "7:40am", "returnDate": "2014-11-12", "returnTime": "10:00am"},
-                                	  {"fltNbr": 1211, "LowestFare": 430.00, "time": "12:00pm", "returnDate": "2014-11-14", "returnTime": "9:00am"},
-                                	  {"fltNbr": 165, "LowestFare": 500.10, "time": "1:50pm", "returnDate": "2014-11-17", "returnTime": "9:00am"}
+                                	  {"fltNbr": 1108, "LowestFare": 493, "time": "7:40 am", "returnDate": "2014-11-11", "returnTime": "6:00 am"},
+                                	  {"fltNbr": 1501, "LowestFare": 643, "time": "8:20 am", "returnDate": "2014-11-12", "returnTime": "7:45 am"},
+                                	  {"fltNbr": 1120, "LowestFare": 701, "time": "11:25 pm", "returnDate": "2014-11-14", "returnTime": "9:25 am"},
+                                	  {"fltNbr": 1138, "LowestFare": 760, "time": "5:50 pm", "returnDate": "2014-11-17", "returnTime": "4:29 pm"}
                                 	 ]		
 					}
 
 function getFlightPrice(destination) {
-	/*
-  for (var idx in priceList.price) {
-	var destCode = priceList.price[idx].destinationLocation;
-	if (destCode == destination) {
-		return priceList.price[idx].fareInfo;
-	}
-  }
-  */
 	if (destination == 'beach') 
 		return beachPriceList;
 	else 
@@ -57,7 +30,7 @@ function buildPriceList(destination) {
   var li = "";
   //container for $li to be added
   $.each(flights.fareInfo, function (i, flight) {
-	  var flightRecord = flight.fltNbr+" "+ flight.LowestFare+" "+ flight.time;
+	  var flightRecord = flight.fltNbr+" "+ flight.LowestFare+" "+ flight.time + " " + flight.returnDate + " " + flight.returnTime;
       //add the <li> to "li" variable
       //note the use of += in the variable
       //meaning I'm adding to the existing data. not replacing it.
